273e1f1102bb3ce6a16a703c175bab631bc7cf57
Julian Pokrovsky <juliankul@gmail.com>
2021-09-17 21:47:01 +0300

    move token to dedic file

--- lox/Main.java
+++ lox/Main.java
@@ -8,24 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class Token {
-   final TokenType type;
-   final String lexeme;
-   final Object literal;
-   final int line;
-
-   Token(TokenType type, String lexeme, Object literal, int line) {
-      this.type = type;
-      this.lexeme = lexeme;
-      this.literal = literal;
-      this.line = line;
-   }
-
-   public String toString() {
-      return type + " " + lexeme + " " + literal;
-   }
-}
-
 public class Main {
    static boolean hadError = false;
 
