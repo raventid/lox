59194c67a3e5a1c60ef60461a22d04f1e93d459e
Julian Pokrovsky <juliankul@gmail.com>
2021-09-18 22:31:59 +0300

    start Scanner implementation

---
+++ lox/Scanner.java
@@ -0,0 +1,29 @@
+package lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lox.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+      while (!isAtEnd()) {
+        // We are at the beginning of the next lexeme.
+        start = current;
+        scanToken();
+      }
+
+      tokens.add(new Token(EOF, "", null, line));
+
+      return tokens;
+    }
+}
