d3b2d6d13f62d14da04f82b70f04b43cf3ea7196
Julian Pokrovsky <juliankul@gmail.com>
2021-10-06 07:39:25 +0300

    update package name

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -21,4 +21,16 @@ public class GenerateAst {
                                                    "Unary    : Token operator, Expr right"
                                                    ));
     }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        writer.println("package loxi;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+        writer.println("}");
+        writer.close();
+    }
 }
