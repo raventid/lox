8beb1b9f61814ee8181b649a551d90db40b250fe
Julian Pokrovsky <juliankul@gmail.com>
2021-10-29 02:45:02 +0300

    slowly implementing parser, add primary skeleton

--- loxi/Parser.java
+++ loxi/Parser.java
@@ -94,9 +94,20 @@ class Parser {
         return primary();
     }
 
-    // THIS IS PLACEHOLDER FOR TESTING PURPOSE
     private Expr primary() {
-        return new Expr.Literal(NUMBER);
+        if (match(FALSE)) { return new Expr.Literal(false); }
+        if (match(TRUE)) { return new Expr.Literal(true); }
+        if (match(NIL)) { return new Expr.Literal(null); }
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
     }
 
     // // // // // // // // // // //
