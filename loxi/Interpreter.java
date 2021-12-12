1d162ec716c9efe8fa0117f84fe59fb6c9e76cc8
Julian Pokrovsky <juliankul@gmail.com>
2021-12-13 02:11:09 +0300

    implement Grouping interpretation

--- loxi/Interpreter.java
+++ loxi/Interpreter.java
@@ -5,4 +5,13 @@ class Interpreter implements Expr.Visitor<Object> {
     public Object visitLiteralExpression(Expr.Literal expression) {
         return expression.value;
     }
+
+    @Override
+    public Object visitGroupingExpression(Expr.Grouping expression) {
+        return evaluate(expression.expression); // not the best naming
+    }
+
+    private Object evaluate(Expr expression) {
+        return expression.accept(this);
+    }
 }
