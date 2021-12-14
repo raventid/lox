2e415e27de4fc261c81ed226539e8891aad61f13
Julian Pokrovsky <juliankul@gmail.com>
2021-12-15 01:09:52 +0300

    partially cover binary expressions

--- loxi/Interpreter.java
+++ loxi/Interpreter.java
@@ -2,16 +2,55 @@ package loxi;
 
 class Interpreter implements Expr.Visitor<Object> {
     @Override
-    public Object visitLiteralExpression(Expr.Literal expression) {
+    public Object visitLiteralExpr(Expr.Literal expression) {
         return expression.value;
     }
 
     @Override
-    public Object visitGroupingExpression(Expr.Grouping expression) {
+    public Object visitGroupingExpr(Expr.Grouping expression) {
         return evaluate(expression.expression); // not the best naming
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expression) {
+        Object right = evaluate(expression.right);
+
+        switch(expression.operator.type) {
+            case MINUS:
+                return -(double)right;
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        // Unreachable
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expression) {
+        Object left = evaluate(expression.left);
+        Object right = evaluate(expression.right);
+
+        switch(expression.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left - (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Unreachable
+        return null;
     }
 
     private Object evaluate(Expr expression) {
         return expression.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) { return false; }
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
     }
 }
