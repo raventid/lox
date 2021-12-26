02a2d4f1f2733ddcb0d02c3591bf35ad142611b2
Julian Pokrovsky <juliankul@gmail.com>
2021-12-26 23:09:11 +0300

    add Interpreter entrypoint

--- loxi/Interpreter.java
+++ loxi/Interpreter.java
@@ -1,6 +1,27 @@
 package loxi;
 
 class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch(RuntimeError error) {
+           // runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+      if (object == null) return "nil";
+      if (object instanceof Double) {
+        String text = object.toString();
+        if (text.endsWith(".0")) {
+          text = text.substring(0, text.length() - 2);
+        }
+        return text;
+      }
+      return object.toString();
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expression) {
         return expression.value;
