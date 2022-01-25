2af724ac7a53390c016d53ac5ffccd87756a3ae6
Julian Pokrovsky <juliankul@gmail.com>
2022-01-25 23:12:29 +0300

    add define() to environment

--- loxi/Environment.java
+++ loxi/Environment.java
@@ -5,4 +5,8 @@ import java.util.Map;
 
 class Environment {
     private final Map<String, Object> values = new HashMap<>();
+
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
 }
