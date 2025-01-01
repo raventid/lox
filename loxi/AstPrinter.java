ee1656538a65f9063ce12967f305b2b61794653a
raventid <juliankul@gmail.com>
2025-01-01 12:45:23 +0800

    update AST printer with new components

--- loxi/AstPrinter.java
+++ loxi/AstPrinter.java
@@ -26,6 +26,16 @@ class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return "UNSUPPORTED_TOKEN";
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return "UNSUPPORTED_TOKEN";
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
         builder.append("(").append(name);
