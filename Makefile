ccce7c88bfa733772399d163155d96ef0688970b
raventid <juliankul@gmail.com>
2025-01-25 23:56:02 +0800

    added logical expressions

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ loxi:
 	javac loxi/Main.java && java loxi/Main
 loxbi:
 	javac loxbi/Main.java && java loxbi/Main
-genexp:
+genast:
 	javac tool/GenerateAst.java && java tool/GenerateAst loxi
 
 ex_closure:
--- loxi/Interpreter.java
+++ loxi/Interpreter.java
@@ -102,6 +102,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+           if (isTruthy(left)) { return left; }
+        } else {
+           if (!isTruthy(left)) { return left; }
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitLiteralExpr(Expr.Literal expression) {
         return expression.value;
     }
