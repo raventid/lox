0a945e5599eb605640061ef0c6a953ffed5c7dad
raventid <juliankul@gmail.com>
2025-04-01 12:31:34 +0800

    tooling: update AST printer with the latest language units

--- loxi/AstPrinter.java
+++ loxi/AstPrinter.java
@@ -17,7 +17,9 @@ class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
 
     @Override
     public String visitLiteralExpr(Expr.Literal expr) {
-        if (expr.value == null) { return "nil"; }
+        if (expr.value == null) {
+            return "nil";
+        }
         return expr.value.toString();
     }
 
@@ -62,6 +64,36 @@ class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
         return "AST_PRINTER_UNSUPPORTED_TOKEN_Var_Statement";
     }
 
+    @Override
+    public String visitReturnStmt(Stmt.Return stmt) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_Return_Statement";
+    }
+
+    @Override
+    public String visitIfStmt(Stmt.If stmt) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_If_Statement";
+    }
+
+    @Override
+    public String visitCallExpr(Expr.Call expr) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_Call_Expression";
+    }
+
+    @Override
+    public String visitWhileStmt(Stmt.While stmt) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_While_Statement";
+    }
+
+    @Override
+    public String visitFunctionStmt(Stmt.Function stmt) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_Function_Statement";
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return "AST_PRINTER_UNSUPPORTED_TOKEN_Logical_Expression";
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
         builder.append("(").append(name);
@@ -75,13 +107,13 @@ class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
 
     // TODO: Move to a separate runner
     // public static void main(String[] args) {
-    //     Expr expression = new Expr.Binary(
-    //         new Expr.Unary(
-    //             new Token(TokenType.MINUS, "-", null, 1),
-    //             new Expr.Literal(123)),
-    //         new Token(TokenType.STAR, "*", null, 1),
-    //         new Expr.Grouping(
-    //             new Expr.Literal(45.67)));
-    //     System.out.println(new AstPrinter().print(expression));
+    // Expr expression = new Expr.Binary(
+    // new Expr.Unary(
+    // new Token(TokenType.MINUS, "-", null, 1),
+    // new Expr.Literal(123)),
+    // new Token(TokenType.STAR, "*", null, 1),
+    // new Expr.Grouping(
+    // new Expr.Literal(45.67)));
+    // System.out.println(new AstPrinter().print(expression));
     // }
 }
