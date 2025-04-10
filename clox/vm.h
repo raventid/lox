aefa942bd5713f191db51e9d51a32a6034102f9f
raventid <juliankul@gmail.com>
2025-04-10 16:12:55 +0800

    clox: add a boilerplate for VM

--- clox/vm.h
+++ clox/vm.h
@@ -3,12 +3,21 @@
 
 #include "chunk.h"
 
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
 typedef struct
 {
     Chunk *chunk;
+    uint8_t *ip;
 } VM;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk *chunk);
 
 #endif
