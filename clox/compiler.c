aabe18b91016bcb001092ddc97f0e61202c10c0e
raventid <juliankul@gmail.com>
2025-04-15 00:00:30 +0800

    clox: add final boilerplate before parsing prefix expr

--- clox/compiler.c
+++ clox/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -9,12 +10,26 @@ typedef struct
     Token current;
     Token previous;
     bool hadError;
+    bool panicMode;
 } Parser;
 
 Parser parser;
 
+Chunk *compilingChunk;
+
+static Chunk *currentChunk()
+{
+    return compilingChunk;
+}
+
 static void errorAt(Token *token, const char *message)
 {
+    if (parser.panicMode)
+    {
+        return;
+    }
+    parser.panicMode = true;
+
     fprintf(stderr, "[line %d] Error", token->line);
 
     if (token->type == TOKEN_EOF)
@@ -61,6 +76,34 @@ static void advance()
 
 static void consume(TokenType type, const char *message)
 {
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte)
+{
+    writeChunk(&currentChunk, byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler()
+{
+    emitReturn();
 }
 
 static void expression()
@@ -70,10 +113,15 @@ static void expression()
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
 
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 
     // int line = -1;
