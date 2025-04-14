2047b055eff074defe1f1b68a9ddbd6da2778dd9
raventid <juliankul@gmail.com>
2025-04-14 22:58:53 +0800

    clox: a boilerplate for parser

--- clox/compiler.c
+++ clox/compiler.c
@@ -4,30 +4,98 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source)
+typedef struct
 {
-    initScanner(source);
+    Token current;
+    Token previous;
+    bool hadError;
+} Parser;
 
-    int line = -1;
+Parser parser;
+
+static void errorAt(Token *token, const char *message)
+{
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR)
+    {
+        // Nothing.
+    }
+    else
+    {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char *message)
+{
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message)
+{
+    errorAt(&parser.current, message);
+}
+
+static void advance()
+{
+    parser.previous = parser.current;
+
     for (;;)
     {
-        Token token = scanToken();
-
-        if (token.line != line)
-        {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else
-        {
-            printf("   | ");
-        }
-
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF)
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR)
         {
             break;
         }
+        errorAtCurrent(parser.current.start);
     }
+}
+
+static void consume(TokenType type, const char *message)
+{
+}
+
+static void expression()
+{
+}
+
+bool compile(const char *source, Chunk *chunk)
+{
+    initScanner(source);
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
+
+    // int line = -1;
+    // for (;;)
+    // {
+    //     Token token = scanToken();
+
+    //     if (token.line != line)
+    //     {
+    //         printf("%4d ", token.line);
+    //         line = token.line;
+    //     }
+    //     else
+    //     {
+    //         printf("   | ");
+    //     }
+
+    //     printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    //     if (token.type == TOKEN_EOF)
+    //     {
+    //         break;
+    //     }
+    // }
 }
