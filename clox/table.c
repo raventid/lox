5dd20bb6d7462af7f1f2ec2be8346f31c4e35ff8
raventid <juliankul@gmail.com>
2025-05-22 22:08:36 +0800

    clox: ch 20.4.5 implementing hashtable tombstones

--- clox/table.c
+++ clox/table.c
@@ -25,11 +25,30 @@ static Entry *findEntry(Entry *entries, int capacity,
                         ObjString *key)
 {
     uint32_t index = key->hash % capacity;
+    Entry *tombstone = NULL;
+
     for (;;)
     {
         Entry *entry = &entries[index];
-        if (entry->key == key || entry->key == NULL)
+        if (entry->key == NULL)
         {
+            if (IS_NIL(entry->value))
+            {
+                // Empty entry.
+                return tombstone != NULL ? tombstone : entry;
+            }
+            else
+            {
+                // We found a tombstone.
+                if (tombstone == NULL)
+                {
+                    tombstone = entry; // we remember first tombstone only, and use it as an insertion position
+                }
+            }
+        }
+        else if (entry->key == key)
+        {
+            // We found the key.
             return entry;
         }
         index = (index + 1) % capacity;
@@ -62,6 +81,7 @@ static void adjustCapacity(Table *table, int capacity)
         entries[i].value = NIL_VAL;
     }
 
+    table->count = 0;
     for (int i = 0; i < table->capacity; i++)
     {
         Entry *entry = &table->entries[i];
@@ -72,6 +92,7 @@ static void adjustCapacity(Table *table, int capacity)
         Entry *dest = findEntry(entries, capacity, entry->key);
         dest->key = entry->key;
         dest->value = entry->value;
+        table->count++;
     }
 
     FREE_ARRAY(Entry, table->entries, table->capacity); // remove previous location
@@ -89,14 +110,36 @@ bool tableSet(Table *table, ObjString *key, Value value)
     }
 
     Entry *entry = findEntry(table->entries, table->capacity, key);
+
     bool isNewKey = entry->key == NULL;
-    if (isNewKey)
+    if (isNewKey && IS_NIL(entry->value)) // only increase count if we are inserting into a new position not a tombstone
     {
         table->count++;
     }
+
     entry->key = key;
     entry->value = value;
     return isNewKey;
+}
+
+bool tableDelete(Table *table, ObjString *key)
+{
+    if (table->count == 0)
+    {
+        return false;
+    }
+
+    // Find the entry.
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL)
+    {
+        return false;
+    }
+
+    // Place a tombstone in the entry.
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
 }
 
 void tableAddAll(Table *from, Table *to)
